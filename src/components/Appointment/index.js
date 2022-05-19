35d7448e6f3af6bbffc1616599eb51aa96e5670c
hseiji <henrique.hst@gmail.com>
2022-05-18 20:54:37 -0400

    update Application.js and DayList component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import 'components/Appointment/styles.scss'
+import { Show } from './Show'
+import { Header } from './Header'
+import { Empty } from './Empty'
 
 export const Appointment = (props) => {
+  console.log(props);
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {props.interview ? <Show student={props.interview.student} interview={props.interview.interviewer.name}/> : <Empty/>}
+    </article>
   )
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -11,6 +11,13 @@ import { DayList } from "components/DayList";
 import { InterviewerListItem } from "components/InterviewerListItem";
 import { InterviewerList } from "components/InterviewerList";
 import { Appointment } from "components/Appointment";
+import { Header } from "components/Appointment/Header";
+import { Empty } from "components/Appointment/Empty";
+import { Show } from "components/Appointment/Show";
+import { Confirm } from "components/Appointment/Confirm";
+import { Status } from "components/Appointment/Status";
+import { Error } from "components/Appointment/Error";
+import { Form } from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -138,9 +145,33 @@ storiesOf("InterviewerList", module)
   ));
 
 
-  storiesOf("Appointment", module)
-    .addParameters({
-      backgrounds: [{ name: "white", value: "#fff", default: true }]
-    })
-    .add("Appointment", () => <Appointment />)
-    .add("Appointment with Time", () => <Appointment time="12pm"/>)
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm"/>)
+  .add("Header", () => <Header time="12pm"/>)
+  .add("Empty", () => (<Empty onAdd={action("onAdd")}/>))
+  .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewers[0].name} onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
+  .add("Confirm", () => <Confirm onConfirm={action("onConfirm")} onCancel={action("onCancel")} message="Delete Appointment?"/>)
+  .add("Status", () => <Status message="Deleting"/>)
+  .add("Error", () => <Error onClose={action("onClose")} message="Could not delete appointment."/>)
+  .add("Edit", () => <Form student="Henry" interviewer={1} interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
+  .add("Create", () => <Form student="" interviewer="" interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
