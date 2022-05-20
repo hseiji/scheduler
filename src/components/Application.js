d566627f181bc7a2e45a9903e498f7b2c4607c04
hseiji <henrique.hst@gmail.com>
2022-05-19 22:24:52 -0400

    add days and setDays useState

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import { DayList } from "./DayList";
 import { Appointment } from "./Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -61,10 +44,19 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
   const appnts = Object.values(appointments).map((app) => {
     return <Appointment key={app.id} {...app}/>
   })
+
+  useEffect(() => {
+    axios.get("/api/days")
+      .then(res => {
+        console.log(res);
+        setDays(res.data)
+      })
+  }, [])
+  
 
   return (
     <main className="layout">
@@ -78,8 +70,8 @@ export default function Application(props) {
       <nav className="sidebar__menu">
       <DayList
         days={days}
-        value={day}
-        onChange={setDay}
+        value={"Monday"}
+        onChange={setDays}
       />
       </nav>
       <img
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -12,7 +12,7 @@ export const DayList = (props) => {
               name={day.name} 
               spots={day.spots} 
               selected={day.name === props.value}
-              setDay={props.onChange}
+              setDays={props.onChange}
             /> 
           )
         })
